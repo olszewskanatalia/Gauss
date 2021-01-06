42ffc6f2c500f30ba1c09ebaa4a4f6bcb241f0ac
Natalia Olszewska <natliaolszewska@gmail.com>
2021-01-06 20:40:32 +0100

    XDDDDDDDDDDD

--- lmp9/trunk/src/backsubst.h
+++ lmp9/trunk/src/backsubst.h
@@ -1,24 +1,10 @@
-#include "backsubst.h"
+#ifndef _BACKSUBST_H_
+#define _BACKSUBST_H_
 
-int backsubst(Matrix *x, Matrix *mat, Matrix *b)
-{
-	if(mat->r != mat->c)
-		return 2;
+#include "mat_io.h"
 
-	int n = x->r;
+int backsubst(Matrix *x, Matrix *mat, Matrix *b);
 
-	for(int row = n - 1; row >= 0; row--)
-	{
-		double sum = 0;
-		
-		for(int column = row + 1; column < n; column++)
-			sum += mat->data[row][column] * x->data[column][0];
-		
-		if(mat->data[row][row] == 0)
-			return 1;
-		
-		x->data[row][0] = (b->data[row][0] - sum) / mat->data[row][row]; 
-	}
+#endif
 
-	return 0;
-}
+
