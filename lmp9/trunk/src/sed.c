3d5951f8ed0903e9947507dc8e8532a459857f19
olamichalska01 <aleksandra.michalska01@gmail.com>
2021-01-07 13:02:50 +0100

    sed.c Ola

--- lmp9/trunk/src/sed.c
+++ lmp9/trunk/src/sed.c
@@ -2,42 +2,41 @@
 
 #include "mat_io.h"
 
-int sed(Matrix *mat, Matrix *b, int kol)
+int sed(Matrix *mat, Matrix *b, int col)
 {
-	int n = b->r;
-	int max = mat->data[kol][kol];
-	int wiersz = kol;
-	int i = kol + 1;
+	int r_num = mat->r;
+	int c_num = mat->c;
+	int max = mat->data[col][col]; // ustawiamy max na kolumne i wiersz o tych samych numerach w macierzy mat
+	int row;
+	int i = col + 1;
 	int k = 0;
+	double tmp, temp;
 
-	for(; i < n; i ++)  // wyszukuje najwiekszej wartosci pod diagonala
+	for(; i < r_num; i++) //najwieksza wartosc pod diagonala
 	{
-		if(max < mat->data[i][kol] && mat->data[i][kol] != 0)
+		if(max < mat->data[i][col] && mat->data[i][col] != 0)
 		{
-			max = mat->data[i][kol];
-			wiersz = i;
+			max = mat->data[i][col]; // zmieniamy max 
+			row = i; // ustawiamy numer wiersza inny niz kolumny
+		
+			for(; k < c_num; k++) //zamiana miejscami w macierzy mat
+			{
+				tmp = mat->data[row][k];
+				mat->data[row][k] = mat->data[col][k];
+				mat->data[col][k] = tmp;
+			}
+			
+			//zamiana miejscami w macierzy b
+			temp = b->data[row][0];
+			b->data[row][0] = b->data[col][0];
+			b->data[col][0] = temp;
+		
 		}
+		else row = col;
 	}
 
-	if(wiersz != kol)   // zamienia wiersze miejscami
-	{
-		for(;k < mat->c; k++)	// dla macierzy
-		{
-			double tmp = mat->data[wiersz][k];
-			mat->data[wiersz][k] = mat->data[kol][k];
-			mat->data[kol][k] = tmp;
-		}
-		// dla prawej strony
-
-		double tnp = b->data[wiersz][0];
-		b->data[wiersz][0] = b->data[kol][0];
-		b->data[kol][0] = tnp;
-
-
-
-	}
-
-	if(mat->data[kol][kol] == 0 ) return 1; // wartosc na diagonali wciaz jest zerem;
+	if(max == 0 ) 
+		return 1; 
 
 	return 0;
 }
