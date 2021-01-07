9b2482f8576faa9f932aa09b2f4f3da50c332a86
olamichalska01 <aleksandra.michalska01@gmail.com>
2021-01-07 12:21:05 +0100

    poprwienie

--- lmp9/trunk/src/backsubst.h
+++ lmp9/trunk/src/backsubst.h
@@ -1,24 +1,6 @@
-#include "backsubst.h"
+#ifndef _BACKSUBST
+#define _BACKSUBST
 
-int backsubst(Matrix *x, Matrix *mat, Matrix *b)
-{
-	if(mat->r != mat->c)
-		return 2;
+int backsubst(Matrix *x, Matrix *mat, Matrix *b);
 
-	int n = x->r;
-
-	for(int row = n - 1; row >= 0; row--)
-	{
-		double sum = 0;
-		
-		for(int column = row + 1; column < n; column++)
-			sum += mat->data[row][column] * x->data[column][0];
-		
-		if(mat->data[row][row] == 0)
-			return 1;
-		
-		x->data[row][0] = (b->data[row][0] - sum) / mat->data[row][row]; 
-	}
-
-	return 0;
-}
+#endif 
